7d1f90371749faa909ecf095a02d8726af298ba1
DESKTOP-65SAUST\Alumno <ascargortageorgy@escivi.com>
2025-02-20 14:56:22 +0100

    Pokedex_SortBy

--- JS/script.js
+++ JS/script.js
@@ -7,18 +7,36 @@ $(document).ready(function (){
     $pokeCont.isotope({
         itemSelector: ".card",
         layoutMode: "fitRows"
+       
+        getSortData: {
+            name: ".name",
+            number: ".number parseInt"
+        }
+
+
     });
 
-    $(".btns-cont button").on("click",function () {
+
+    $(".btns-cont .filter").on("click",function (){
 
         let filterVal = $(this).data("filter");
         
-        $pokeCont.isotope({
+        $pokeCont.isotope ({
             filter: filterVal
+
         })
+
     });
 
 
+    $(".btns-cont .sort").on("click",function (){
+        let sortByValue = $(this).data("sortby");
+        
+        $pokeCont.isotope ({
+            sortBy: sortByValue
+
+        })
+    })
 
 
 
