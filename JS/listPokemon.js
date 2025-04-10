e3703e6228f5726daff7922c75174489e0afba24
DESKTOP-65SAUST\Alumno <informatica@esne.es>
2025-04-10 15:51:48 +0200

    Dex

--- JS/listPokemon.js
+++ JS/listPokemon.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
+    // Array donde se irán guardando los pokemon de forma ordenada por ID
+    let sortedPokemon = [];
+
+
+
+
 
     // Primera llamada a la API para conseguir el listado.
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=251")
         
         .then(function (response) {
             return response.json();
@@ -11,11 +17,11 @@ $(document).ready(function(){
             // console.log(result);
             let pokemonList = result.results;
 
-           // Lanzamos una nueva petición a la API por cada pokemon del primer listado que hemos recibido
-           pokemonList.forEach(function (pokemon){
-                fetchPokemonDetails(pokemon);
+            // Lanzamos una nueva petición a la API por cada pokemon del primer listado que hemos recibido
+            pokemonList.forEach(function (pokemon){
+                fetchAndSortPokemon(pokemon);
 
-           });
+            });
 
         })
         .catch(function(err) {
@@ -23,9 +29,10 @@ $(document).ready(function(){
            
         });
         
-        // Funcion que se encarga de pedir los datos de cada Pokemon
 
-        function fetchPokemonDetails(pokemon){
+        // Función que pide a la API los datos de un Pokémon y los va ordenando en un array
+        function fetchAndSortPokemon(pokemon){
+            
             let urlPokemon = pokemon.url;
             // console.log(urlPokemon);
 
@@ -36,31 +43,135 @@ $(document).ready(function(){
                     return response.json();
 
                 })
+
                 .then(function(pokemonDetails) {
                     console.log(pokemonDetails);
 
-                    let nameCap = pokemonDetails.name.charAt(0).toUpperCase() + pokemonDetails.name.slice(1);
-                    
-                    let pokeHTML = `
-                    <div class="card" style="width: 18rem;">
-                        <img src="${pokemonDetails.sprites.front_shiny}" class="card-img-top" alt="...">
-                        <div class="card-body justify-content-center">
-                            <h5 class="card-title">${nameCap}</h5>
-                            <p class="card-text">${pokemonDetails.id}</p>
-                            <a href="https://www.wikidex.net/wiki/${nameCap}" class="btn btn-primary">Wiki Data</a>
-                        </div>
-                    </div>
-                    `;
-                    $("#pokeCont").append(pokeHTML);
+                    // Insertamos el pokemon con sus datos en el array sortedPokemon
+                    sortedPokemon.push(pokemonDetails);
+                    sortedPokemon.sort(function (a,b){
+                        return a.id - b.id; // Ordenar por ID en orden ascendente
+                        
+                    });
 
-
-
+                    //llamamos a la función para renderizar el HTML de cada card
+                    renderPokemonCard();
 
                 })
+
                 .catch(function (err) {
                     console.log(err);
                     
                 });
-        }       
+        }
+
+        // Función para renderizar los datos de cada pokemon en un card de BS
+        // Esta funcion recorrerá el array sortedPokemon
+
+
+        function renderPokemonCard() {
+            $("#pokeCont").empty();
+
+            sortedPokemon.forEach(function (pokemonDetails){
+                let nameCap = pokemonDetails.name.charAt(0).toUpperCase() + pokemonDetails.name.slice(1);
+
+                if (nameCap === "Nidoran-f" || nameCap === "Nidoran-m"){ 
+                    nameCap = "Nidoran";
+                }
+                
+                // si pokemonDetails.name = nidoran-f 
+1
+                        
+                let pokeHTML = `
+                    <div class="card" style="width: 18rem;">
+                        <img src="${pokemonDetails.sprites.front_shiny}" class="card-img-top" alt="...">
+                        <div class="card-body d-felx justify-content-center">
+                            <h5 class="card-title d-flex justify-content-center">${nameCap}</h5>
+                            <p class="card-text text-center">${pokemonDetails.id}</p>
+                            <a href="https://www.wikidex.net/wiki/${nameCap}" target="_blank" class="btn btn-primary d-flex justify-content-center">Wiki Data</a>
+                        </div>
+                    </div>
+                `;
+    
+                $("#pokeCont").append(pokeHTML);
+            });
+            
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Funcion que se encarga de pedir los datos de cada Pokemon
+
+        // function fetchPokemonDetails(pokemon){
+        //     let urlPokemon = pokemon.url;
+        //     // console.log(urlPokemon);
+
+        //     // Con esta llamada pedimos los detalles de cada Pokemon
+
+        //     fetch(urlPokemon)
+        //         .then(function (response) {
+        //             return response.json();
+
+        //         })
+
+        //         .then(function(pokemonDetails) {
+        //             console.log(pokemonDetails);
+
+        //             let nameCap = pokemonDetails.name.charAt(0).toUpperCase() + pokemonDetails.name.slice(1);
+                    
+        //             let pokeHTML = `
+        //             <div class="card" style="width: 18rem;">
+        //                 <img src="${pokemonDetails.sprites.front_shiny}" class="card-img-top" alt="...">
+        //                 <div class="card-body d-felx justify-content-center">
+        //                     <h5 class="card-title d-flex justify-content-center">${nameCap}</h5>
+        //                     <p class="card-text text-center">${pokemonDetails.id}</p>
+        //                     <a href="https://www.wikidex.net/wiki/${nameCap}" target="_blank" class="btn btn-primary d-flex justify-content-center">Wiki Data</a>
+        //                 </div>
+        //             </div>
+        //             `;
+        //             $("#pokeCont").append(pokeHTML);
+                    
+
+        //         })
+
+        //         .catch(function (err) {
+        //             console.log(err);
+                    
+        //         });
+
+        // }       
 
 });
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Pokedex_13_02_25_DWA
  
+- Convertir la primera letra del nombre de cada pokemon en mayuscula
+- Crear botones de páginas que lancen la función listPokemon.js.
+    PISTA -> mirar en la docu pokeAPI las instrucciones de paginación.
+
+- Añadir un spinner (loading) de BS al #pokeCont
+
+- Volver a activar y que funcione IsotopeJS
